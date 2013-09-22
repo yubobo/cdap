7a97efeffb4cbc30e41333ce638974ba724b2f1c
Don Mosites <don@continuuity.com>
2013-09-21 19:53:16 -0700

    Couple of corrections

--- web-app/client/core/controllers/workflow-history.js
+++ web-app/client/core/controllers/workflow-history.js
@@ -25,11 +25,11 @@ define([], function () {
         if ('mapReduceName' in model.actions[i].options) {
           var transformedModel = C.Batch.transformModel(model.actions[i]);
 
-          this.get('elements.Actions.content').push(C.Batch.create(transformedModel));      
+          this.get('elements.Actions.content').push(C.Batch.create(transformedModel));
         } else {
-          this.get('elements.Actions.content').push(Em.Object.create(model.actions[i]));        
+          this.get('elements.Actions.content').push(Em.Object.create(model.actions[i]));
         }
-        
+
       }
 
       this.HTTP.rest('apps', model.app, 'workflows', model.name, 'history', function (response) {
@@ -44,6 +44,12 @@ define([], function () {
           }
 
       });
+
+    },
+
+    loadRun: function () {
+
+      // Noop
 
     },
 
