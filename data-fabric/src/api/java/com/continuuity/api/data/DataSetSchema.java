58187157777d8645eb6183d49c781cbef1e8f7fc
Gary Helmling <gary@continuuity.com>
2013-12-05 16:24:00 -0800

    Add Hive integration module

--- data-fabric/src/api/java/com/continuuity/api/data/DataSetSchema.java
+++ data-fabric/src/api/java/com/continuuity/api/data/DataSetSchema.java
@@ -3,7 +3,8 @@ package com.continuuity.api.data;
 import java.util.List;
 
 /**
- *
+ * DataSets which allow integration with external map reduce processing need to implement this interface in order
+ * to allow Hive / HCatalog to obtain the correct schema information for reading records.
  */
 public interface DataSetSchema {
   public List<DataSetTable> listTables();
