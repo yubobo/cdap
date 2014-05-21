36cde15a6df7253cddbed6340ee55643edc94169
Alvin Wang <alvin@continuuity.com>
2014-05-20 17:35:19 -0700

    zk fix for DatasetManagerServiceClient

--- data-fabric/src/main/java/com/continuuity/data2/datafabric/dataset/client/DatasetManagerServiceClient.java
+++ data-fabric/src/main/java/com/continuuity/data2/datafabric/dataset/client/DatasetManagerServiceClient.java
@@ -40,14 +40,13 @@ import javax.annotation.Nullable;
 public class DatasetManagerServiceClient {
   private static final Gson GSON = new Gson();
 
+  private final DiscoveryServiceClient discoveryClient;
+
   private EndpointStrategy endpointStrategy;
 
   @Inject
   public DatasetManagerServiceClient(DiscoveryServiceClient discoveryClient) {
-
-    this.endpointStrategy = new TimeLimitEndpointStrategy(
-      new RandomEndpointStrategy(discoveryClient.discover(Constants.Service.DATASET_MANAGER)),
-      1L, TimeUnit.SECONDS);
+    this.discoveryClient = discoveryClient;
   }
 
   public DatasetInstanceMeta getInstance(String instanceName) throws DatasetManagementException {
@@ -232,9 +231,13 @@ public class DatasetManagerServiceClient {
   }
 
   private String resolve(String resource) {
+    if (endpointStrategy == null) {
+      this.endpointStrategy = new TimeLimitEndpointStrategy(
+        new RandomEndpointStrategy(discoveryClient.discover(Constants.Service.DATASET_MANAGER)),
+        1L, TimeUnit.SECONDS);
+    }
     InetSocketAddress addr = this.endpointStrategy.pick().getSocketAddress();
-    return String.format("http://%s:%s/%s/datasets/%s", addr.getHostName(), addr.getPort(),
-                         Constants.Dataset.Manager.VERSION, resource);
+    return String.format("http://%s:%s/%s/datasets/%s", addr.getHostName(), addr.getPort(), Constants.Dataset.Manager.VERSION, resource);
   }
 
   private final class HttpResponse {
