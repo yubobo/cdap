35699c85dccb1c1fcba9ddef59e142e77b935160
Julien Guery <julien@continuuity.com>
2014-05-31 11:51:46 -0700

    Logs in gradle tests

--- explore/src/test/java/com/continuuity/hive/HiveClientTestUtils.java
+++ explore/src/test/java/com/continuuity/hive/HiveClientTestUtils.java
@@ -3,20 +3,24 @@ package com.continuuity.hive;
 import com.continuuity.hive.client.HiveClient;
 
 import junit.framework.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
 /**
- *
+ * Utility class for hive tests.
  */
 public class HiveClientTestUtils {
+  private static final Logger LOG = LoggerFactory.getLogger(HiveClientTestUtils.class);
 
   public static void assertCmdFindPattern(HiveClient hiveClient, String cmd, String pattern) throws IOException {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     hiveClient.sendCommand(cmd, out, null);
     String res = out.toString("UTF-8");
+    LOG.error("Command result {}", res);
     try {
       Assert.assertTrue(Pattern.compile(pattern, Pattern.DOTALL).matcher(res).find());
     } finally {
