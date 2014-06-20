238dcf54e2ca9402ca257d3d1ebff9a56646d12e
rohit <nrohits@gmail.com>
2014-06-20 12:46:46 -0700

    beginning graph stuff

--- web-app/client/core/angular/directives/sparkline.js
+++ web-app/client/core/angular/directives/sparkline.js
@@ -6,132 +6,87 @@ define(function() {
 
   var Directive = [function() {
   
+    function displayGraphExample (elm, width, height, interpolation, animate, updateDelay, transitionDelay) {
+      // create an SVG element inside the #graph div that fills 100% of the div
+      var graph = d3.select(elm).append("svg:svg").attr("width", "100%").attr("height", "100%");
+
+      // create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
+      var data = [3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 9];
+
+      // X scale will fit values from 0-10 within pixels 0-100
+      var x = d3.scale.linear().domain([0, 48]).range([-5, width]); // starting point is -5 so the first value doesn't show and slides off the edge as part of the transition
+      // Y scale will fit values from 0-10 within pixels 0-100
+      var y = d3.scale.linear().domain([0, 10]).range([0, height]);
+
+      // create a line object that represents the SVN line we're creating
+      var line = d3.svg.line().interpolate("monotone")
+        // assign the X function to plot our line as we wish
+        .x(function(d,i) { 
+          // verbose logging to show what's actually being done
+          //console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
+          // return the X coordinate where we want to plot this datapoint
+          return x(i); 
+        })
+        .y(function(d) { 
+          // verbose logging to show what's actually being done
+          //console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
+          // return the Y coordinate where we want to plot this datapoint
+          return y(d); 
+        })
+        .interpolate(interpolation);
+    
+      // display the line by appending an svg:path element with the data line we created above
+      graph.append("svg:path").attr("d", line(data));
+      // or it can be done like this
+      //graph.selectAll("path").data([data]).enter().append("svg:path").attr("d", line);
+      
+      
+      function redrawWithAnimation() {
+        // update with animation
+        graph.selectAll("path")
+          .data([data]) // set the new data
+          .attr("transform", "translate(" + x(1) + ")") // set the transform to the right by x(1) pixels (6 for the scale we've set) to hide the new value
+          .attr("d", line) // apply the new data values ... but the new value is hidden at this point off the right of the canvas
+          .transition() // start a transition to bring the new value into view
+          .ease("linear")
+          .duration(transitionDelay) // for this demo we want a continual slide so set this to the same as the setInterval amount below
+          .attr("transform", "translate(" + x(0) + ")"); // animate a slide to the left back to x(0) pixels to reveal the new value
+          
+          /* thanks to 'barrym' for examples of transform: https://gist.github.com/1137131 */
+      }
+      
+      function redrawWithoutAnimation() {
+        // static update without animation
+        graph.selectAll("path")
+          .data([data]) // set the new data
+          .attr("d", line); // apply the new data values
+      }
+      
+      setInterval(function() {
+         var v = data.shift(); // remove the first element of the array
+         data.push(v); // add a new element to the array (we're just taking the number we just shifted off the front and appending to the end)
+         if(animate) {
+           redrawWithAnimation();
+         } else {
+             redrawWithoutAnimation();
+         }
+      }, updateDelay);
+    }
+
     return {
       
       restrict: 'AE',
-      template: '<div class="sparkline-list-container">',
-      link: function(scope, elm, attrs) {
-        scope.$watch('collect', function (newVal, oldVal) {
-          if (angular.isArray(newVal.data) && newVal.data.length) {
-            console.log(newVal);
+      scope: {
+        data: "="
+      },
+      template: '<div class="sparkline-list-container"></div>',
+      link: function (scope, elm, attrs) {
+        displayGraphExample(elm[0], 300, 30, "basis", true, 1000, 1000);
+        scope.$watch('data', function (newVal, oldVal) {
+          if (newVal && angular.isArray(newVal) && newVal.length) {
           }
         });
-        var percent = null;
-        var shade = null;
-        var data = [];
-        var widget = jQuery(elm[0]);
-        var w = 300;
-        var h = 150;
-        var allData = [], length = 0;
-        var max = d3.max(allData) || 9;
-        var min = d3.min(allData) || -1;
-        var extend = Math.round(w / data.length);
-
-        var margin = 5;
-        var yBuffer = 0.0;
-        var y, x;
-
-        x = d3.scale.linear();//.domain([0, data.length]).range([0, w]);
-        y = d3.scale.linear();
-
-        var vis = widget
-          .append("svg:svg")
-          .attr('width', '100%')
-          .attr('height', '100%')
-          .attr('preserveAspectRatio', 'none');
-
-        var g = vis.append("svg:g");
-        var line = d3.svg.line().interpolate("monotone")
-          .x(function(d,i) { return x(i); })
-          .y(function(d) { return y(d); });
-
-        if (percent || shade) {
-          var area = d3.svg.area()
-            .x(line.x())
-            .y1(line.y())
-            .y0(y(0));
-          g.append("svg:path").attr('class', 'sparkline-area').attr("d", area(data));
-        }
-
-        g.append("svg:path").attr('class', 'sparkline-data').attr("d", line(data));
-
-        return {
-          g: g,
-          percent: percent,
-          shade: shade,
-          update: function (name, data) {
-
-            this.series[name] = data;
-
-            var allData = [], length = 0;
-            for (var i in this.series) {
-              allData = allData.concat(this.series[i]);
-              if (this.series[i].length > length) {
-                length = this.series[i].length;
-              }
-            }
-            var max = d3.max(allData) || 100;
-            var min = d3.min(allData) || 0;
-            var extend = Math.round(w / data.length);
-
-            var yBuffer = 0.0;
-            var y, x;
-
-            x = d3.scale.linear().domain([0, length]).range([0 - extend, w - extend]);
-
-            if (this.percent) {
-              y = d3.scale.linear()
-                .domain([100, 0])
-                .range([margin, h - margin]);
-            } else {
-              if ((max - min) === 0) {
-                if (data[0]) {
-                  max = data[0] + data[0] * 0.1;
-                  min = data[0] - data[0] * 0.1;
-                } else {
-                  max = 10;
-                  min = 0;
-                }
-              }
-              y = d3.scale.linear()
-                .domain([max + (max * yBuffer), min - (min * yBuffer)])
-                .range([margin, h - margin]);
-            }
-
-
-            var line = d3.svg.line().interpolate("monotone")
-              .x(function(d,i) { return x(i); })
-              .y(function(d) { return y(d); });
-
-            if (this.percent || this.shade) {
-              var area = d3.svg.area().interpolate("monotone")
-                .x(line.x())
-                .y1(line.y())
-                .y0(y(-100));
-
-              this.g.selectAll("path.sparkline-area")
-                .data([data])
-                .attr("transform", "translate(" + x(0) + ")")
-                .attr("d", area)
-                .transition()
-                .ease("linear")
-                .duration(C.POLLING_INTERVAL)
-                .attr("transform", "translate(" + x(-(C.POLLING_INTERVAL / 1000)) + ")");
-            }
-
-            this.g.selectAll("path.sparkline-data")
-              .data([data])
-              .attr("transform", "translate(" + x(0) + ")")
-              .attr("d", line)
-              .transition()
-              .ease("linear")
-              .duration(C.POLLING_INTERVAL)
-              .attr("transform", "translate(" + x(-(C.POLLING_INTERVAL / 1000)) + ")");
-
-          }
-        };
       }
-    
     }
   
   }];
