b11e0cbe91360c0f93eef5097f72129976704490
Alvin Wang <alvin@continuuity.com>
2014-07-17 13:56:57 -0700

    Reactor Java client library

---
+++ reactor-client/src/test/java/com/continuuity/reactor/client/ReactorQueryClientTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2012-2014 Continuuity, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.continuuity.reactor.client;
+
+import com.continuuity.reactor.client.app.FakeApp;
+import com.continuuity.reactor.client.common.ReactorClientTestBase;
+import com.continuuity.reactor.client.config.ReactorClientConfig;
+import com.continuuity.reactor.metadata.ColumnDesc;
+import com.continuuity.reactor.metadata.QueryHandle;
+import com.continuuity.reactor.metadata.QueryResult;
+import com.continuuity.reactor.metadata.QueryStatus;
+import org.junit.Assert;
+import org.junit.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class ReactorQueryClientTest extends ReactorClientTestBase {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ReactorQueryClientTest.class);
+
+  private ReactorAppClient appClient;
+  private ReactorQueryClient queryClient;
+
+  @Before
+  public void setUp() throws Throwable {
+    super.setUp();
+
+    ReactorClientConfig config = new ReactorClientConfig("localhost");
+    appClient = new ReactorAppClient(config);
+    queryClient = new ReactorQueryClient(config);
+  }
+
+//  @Test
+  public void testAll() throws Exception {
+    appClient.deploy(createAppJarFile(FakeApp.class));
+
+    QueryHandle queryHandle = queryClient.execute("select * from continuuity_user_" + FakeApp.DS_NAME);
+    QueryStatus status = new QueryStatus(null, false);
+
+    long startTime = System.currentTimeMillis();
+    // TODO: refactor
+    while ("RUNNING".equals(status.getStatus()) || "INITIALIZED".equals(status.getStatus())
+      || "PENDING".equals(status.getStatus())) {
+      Thread.sleep(1000);
+      status = queryClient.getStatus(queryHandle);
+    }
+
+    Assert.assertTrue(status.hasResults());
+
+    List<ColumnDesc> schema = queryClient.getSchema(queryHandle);
+    String[] header = new String[schema.size()];
+    for (int i = 0; i < header.length; i++) {
+      ColumnDesc column = schema.get(i);
+      // Hive columns start at 1
+      int index = column.getPosition() - 1;
+      header[index] = column.getName() + ": " + column.getType();
+    }
+    List<QueryResult> results = queryClient.getResults(queryHandle, 20);
+
+    queryClient.delete(queryHandle);
+  }
+}
---
+++ reactor-client/src/test/java/com/continuuity/reactor/client/common/ReactorClientTestBase.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2012-2014 Continuuity, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.continuuity.reactor.client.common;
+
+import com.continuuity.reactor.client.ReactorProgramClient;
+import com.continuuity.reactor.client.exception.NotFoundException;
+import com.continuuity.reactor.client.exception.ProgramNotFoundException;
+import com.continuuity.reactor.metadata.ProgramRecord;
+import com.continuuity.reactor.metadata.ProgramType;
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ */
+public abstract class ReactorClientTestBase extends SingleNodeTestBase {
+
+  protected void verifyProgramNames(List<String> expected, List<ProgramRecord> actual) {
+    Assert.assertEquals(expected.size(), actual.size());
+    for (ProgramRecord actualProgramRecord : actual) {
+      Assert.assertTrue(expected.contains(actualProgramRecord.getId()));
+    }
+  }
+
+  protected void assertProcedureInstances(ReactorProgramClient programClient, String appId, String procedureId,
+                                          int numInstances) throws IOException, NotFoundException {
+    int actualInstances;
+    int numTries = 0;
+    int maxTries = 5;
+    do {
+      actualInstances = programClient.getProcedureInstances(appId, procedureId);
+      numTries++;
+    } while (actualInstances != numInstances && numTries <= maxTries);
+    Assert.assertEquals(numInstances, actualInstances);
+  }
+
+  protected void assertFlowletInstances(ReactorProgramClient programClient, String appId, String flowId,
+                                        String flowletId, int numInstances) throws IOException, NotFoundException {
+    int actualInstances;
+    int numTries = 0;
+    int maxTries = 5;
+    do {
+      actualInstances = programClient.getFlowletInstances(appId, flowId, flowletId);
+      numTries++;
+    } while (actualInstances != numInstances && numTries <= maxTries);
+    Assert.assertEquals(numInstances, actualInstances);
+  }
+
+  protected void assertProgramRunning(ReactorProgramClient programClient, String appId, ProgramType programType,
+                                      String programId) throws IOException, ProgramNotFoundException {
+    assertProgramStatus(programClient, appId, programType, programId, "RUNNING");
+  }
+
+
+  protected void assertProgramStopped(ReactorProgramClient programClient, String appId, ProgramType programType,
+                                      String programId) throws IOException, ProgramNotFoundException {
+    assertProgramStatus(programClient, appId, programType, programId, "STOPPED");
+  }
+
+  protected void assertProgramStatus(ReactorProgramClient programClient, String appId, ProgramType programType,
+                                     String programId, String programStatus)
+    throws IOException, ProgramNotFoundException {
+
+    String status;
+    int numTries = 0;
+    int maxTries = 5;
+    do {
+      status = programClient.getStatus(appId, programType, programId);
+      numTries++;
+    } while (!status.equals(programStatus) && numTries <= maxTries);
+    Assert.assertEquals(programStatus, status);
+  }
+
+}
