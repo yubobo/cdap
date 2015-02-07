61903ba21bf66f358779eabe2e4b65f52d0ee796
Parth Gandhi <parth@cask.co>
2015-02-06 21:20:34 -0800

    First working version

--- cdap-explore/src/main/java/co/cask/cdap/explore/security/HiveSecureStoreUpdater.java
+++ cdap-explore/src/main/java/co/cask/cdap/explore/security/HiveSecureStoreUpdater.java
@@ -16,15 +16,24 @@
 
 package co.cask.cdap.explore.security;
 
+import co.cask.cdap.api.common.Bytes;
 import co.cask.cdap.data.security.HBaseTokenUtils;
 import com.google.common.base.Throwables;
+import com.google.common.net.HostAndPort;
 import com.google.inject.Inject;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.ClientCache;
+import org.apache.hadoop.mapred.ResourceMgrDelegate;
+import org.apache.hadoop.mapreduce.v2.api.MRClientProtocol;
+import org.apache.hadoop.mapreduce.v2.api.protocolrecords.GetDelegationTokenRequest;
+import org.apache.hadoop.mapreduce.v2.api.protocolrecords.impl.pb.GetDelegationTokenRequestPBImpl;
 import org.apache.hadoop.security.Credentials;
 import org.apache.hadoop.security.UserGroupInformation;
-import org.apache.hadoop.yarn.api.records.Token;
+import org.apache.hadoop.security.token.Token;
+import org.apache.hadoop.security.token.TokenIdentifier;
 import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnException;
 import org.apache.hadoop.yarn.util.ConverterUtils;
 import org.apache.twill.api.RunId;
@@ -37,6 +46,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -57,6 +67,11 @@ public class HiveSecureStoreUpdater implements SecureStoreUpdater {
     this.credentials = new Credentials();
   }
 
+  private void printToken(String type, Token token) {
+    LOG.info("!!{} token: {}, {}, {}",
+             type, token.getService(), Bytes.toString(token.getIdentifier()), token.getKind());
+  }
+
   private void refreshCredentials() {
     try {
       Credentials creds = new Credentials();
@@ -67,19 +82,76 @@ public class HiveSecureStoreUpdater implements SecureStoreUpdater {
       YarnClient yarnClient = YarnClient.createYarnClient();
       yarnClient.init(conf);
       yarnClient.start();
-      Token token = yarnClient.getRMDelegationToken(new Text(UserGroupInformation.getCurrentUser().getShortUserName()));
+      org.apache.hadoop.security.token.Token<TokenIdentifier> rmToken =
+        ConverterUtils.convertFromYarn(yarnClient.getRMDelegationToken(
+          new Text(UserGroupInformation.getCurrentUser().getShortUserName())), YarnUtils.getRMAddress(conf));
+
       yarnClient.stop();
 
-      creds.addToken(new Text(token.getService()), ConverterUtils.convertFromYarn(token, YarnUtils.getRMAddress(conf)));
-      creds.addToken(new Text(token.getService()), ConverterUtils.convertFromYarn(token, YarnUtils.getRMAddress(conf)));
+      printToken("RMToken", rmToken);
+
+
+      creds.addToken(new Text(rmToken.getService()), rmToken);
+
+      MRClientCache clientCache = new MRClientCache(conf, new ResourceMgrDelegate(new YarnConfiguration(conf)));
+      MRClientProtocol hsProxy = clientCache.getInitializedHSProxy();
+      GetDelegationTokenRequest request = new GetDelegationTokenRequestPBImpl();
+//      request.setRenewer(UserGroupInformation.getCurrentUser().getShortUserName());
+      request.setRenewer("yarn");
+
+      LOG.info("!!! Credentials before");
+      for (Token t : creds.getAllTokens()) {
+        LOG.info("{}", t);
+      }
+
+      try {
+        String historyServerAddress = conf.get("mapreduce.jobhistory.address");
+        LOG.info("HostAndPort for HistoryServer: {}", historyServerAddress);
+        HostAndPort hostAndPort = HostAndPort.fromString(historyServerAddress);
+        org.apache.hadoop.security.token.Token<TokenIdentifier> hsToken =
+          ConverterUtils.convertFromYarn(hsProxy.getDelegationToken(request).getDelegationToken(),
+                                         new InetSocketAddress(hostAndPort.getHostText(), hostAndPort.getPort())
+        );
+
+        printToken("MRToken", hsToken);
+
+        LOG.info("HSToken Name: {}", hsToken.getService());
+        creds.addToken(new Text(hsToken.getService()), hsToken);
+
+      } catch (Throwable t) {
+        LOG.error("!!! Got error for HS:", t);
+      }
+
+      LOG.info("!!! Credentials after");
+      for (Token t : creds.getAllTokens()) {
+        LOG.info("{}", t);
+      }
+
+//      // Job History token shit
+//
+//      try {
+//        JobHistory jobHistory = new JobHistory();
+//        HistoryServerStateStoreService store = HistoryServerStateStoreServiceFactory.getStore(conf);
+//        JHSDelegationTokenSecretManager tokenSecretManager =
+//          new JHSDelegationTokenSecretManager(24 * 60 * 60 * 1000, 7 * 24 * 60 * 60 * 1000, 24 * 60 * 60 * 1000,
+//                                              3600000, store);
+//
+//        HistoryClientService historyClientService =
+//          new HistoryClientService((HistoryContext) jobHistory, tokenSecretManager);
+//        tokenSecretManager.startThreads();
+//
+//        GetDelegationTokenRequestPBImpl delegationTokenRequestPB = new GetDelegationTokenRequestPBImpl();
+//        delegationTokenRequestPB.setRenewer(UserGroupInformation.getCurrentUser().getShortUserName());
+//        Token delegationToken =
+//          historyClientService.getClientHandler().getDelegationToken(delegationTokenRequestPB).getDelegationToken();
+//        creds.addToken(new Text(delegationToken.getService()),
+//                       ConverterUtils.convertFromYarn(delegationToken, YarnUtils.getRMAddress(conf)));
+//
+//      } catch (Throwable t) {
+//        LOG.info("!!! Got exception for HistoryClientService: {}", t);
+//      }
 
       YarnUtils.addDelegationTokens(conf, locationFactory, creds);
-
-      JobHistory jobHistoryService = new JobHistory();
-
-
-
-
       this.credentials = creds;
     } catch (IOException ioe) {
       throw Throwables.propagate(ioe);
@@ -108,5 +180,18 @@ public class HiveSecureStoreUpdater implements SecureStoreUpdater {
       refreshCredentials();
     }
     return YarnSecureStore.create(credentials);
+  }
+
+  private static class MRClientCache extends ClientCache {
+
+    public MRClientCache(Configuration conf, ResourceMgrDelegate rm) {
+      super(conf, rm);
+    }
+
+    @Override
+    public synchronized MRClientProtocol getInitializedHSProxy() throws IOException {
+      return super.getInitializedHSProxy();
+    }
+
   }
 }
